c8b759678f2cecd8540969f01ee4d0aa7f510085
Tomas-caula <70955134+Tomas-caula@users.noreply.github.com>
2020-10-08 14:02:05 -0300

    .
    .

--- WindowsFormsApp3/Imagen.cs
+++ WindowsFormsApp3/Imagen.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,31 @@ namespace WindowsFormsApp3
 {
     class Imagen
     {
+    }
+    public class BaseDatos {
+        // Creacion de la constante que va a Delimitar el cambio entre un Fila y Otra = (#!"#$%&/¡) 
+        //private const string DEL = "(#!#$%&/¡)";
+        // RUTA 
+        //private string _ruta = Application.StartupPath + "\\db\\";
+        // Configuara nombre de la tabla 
+        //public string Tabla(get; set;)}
+        //public BaseDatos(string t) { Tabla = t; }
+
+
+        //EVENTOS 
+        // Separar con el DEL (Deliitador)
+        // private string [] splitDEL (String txt)
+        // {
+        //  return txt.Split(new string[] { DEL }, StringSplitOptions.None);
+        // }
+        // Hacer salto entre Linea 
+        // private string[] splitLINEA(String txt)
+        // {
+        //   return txt.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+        //  }
+        // private void Carpeta () { if (!Directory.Exists(_ruta)) Directory.CreateDirectory(_ruta); }
+        //  private string Ruta() { return _ruta + Tabla + ".gdb";  }
+        // EVENTOS 
+
     }
 }
