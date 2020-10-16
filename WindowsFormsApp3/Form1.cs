98c2d9a03ca02e3080b0878ee66cab268deac5a3
Tomas-caula <70955134+Tomas-caula@users.noreply.github.com>
2020-10-16 16:27:00 -0300

    Actualizacion BaseData
    Agrege El ReadMyData
    Cree la Conexion con el Accdb
    Todo con sus //
    Agrege un InsertRow

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -15,11 +15,59 @@ namespace Picto
 {
     public partial class Home : Form
     {
-        //
+        public void ReadMyData(string connectionString)
+        {
+            string queryString = "SELECT * FROM Table1";
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                OleDbCommand command = new OleDbCommand(queryString, connection);
+                connection.Open();
+                OleDbDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetInt32(0) + ", " + reader.GetString(1));
+                }
+                // always call Close when done reading.
+                reader.Close();
+            }
+        }
+
+
+        // Creacion de la Conexion 
         OleDbConnection cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0 Data Source =BaseDataPicto.accdb");
-        
+        // Conexion llamada cn; 
+
         //
-        public Home()
+        public void InsertRow(string connectionString, string insertSQL)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                // The insertSQL string contains a SQL statement that
+                // inserts a new row in the source table.
+                OleDbCommand command = new OleDbCommand(insertSQL);
+
+                // Set the Connection to the new OleDbConnection.
+                command.Connection = connection;
+
+                // Open the connection and execute the insert command.
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                // The connection is automatically closed when the
+                // code exits the using block.
+            }
+        }
+            //
+
+
+            public Home()
         {
             InitializeComponent();
         }
@@ -57,6 +105,7 @@ namespace Picto
         {
 
         }
+    
     }
 }
 // Variable Global 
@@ -66,3 +115,9 @@ public static class MyGlobals
     public static string Texto;
 }
 // fin MyGlobals 
+
+
+
+
+
+
