39a49bd6c42d5be7639aec60e40301c407c27054
Tomas-caula <70955134+Tomas-caula@users.noreply.github.com>
2020-11-02 13:10:06 -0300

    .
    .

--- WindowsFormsApp3/Pictogramas1.cs
+++ WindowsFormsApp3/Pictogramas1.cs
@@ -70,8 +70,7 @@ namespace Picto
 
         private void vozopictograma_Load(object sender, EventArgs e)
         {
-            
-
+    
 
         }
 
@@ -154,7 +153,9 @@ namespace Picto
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0 Data Source = BaseDataPicto.accdb");
+            OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = BaseDataPicto.accdb");
+            OleDbDataAdapter da;
+
             con.Open();
             OleDbCommand cmd = new OleDbCommand("select Id, Nombre from Tabla1", con);
             OleDbDataReader rd = cmd.ExecuteReader();
